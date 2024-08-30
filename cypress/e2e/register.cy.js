a91e2db782112f61e080208ccf81ec9127ac0f95
Gerfson Alves <gerfson17@hotmail.com>
2024-08-30 17:24:06 -0300

    Finalizando login, compra e logout

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,23 +1,21 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  beforeEach('visitando o site', () => {
-    cy.visit('https://www.saucedemo.com')
+describe('Template Spec - Testes de Cadastro e Fluxo de Compra', () => {
 
-  })
+  beforeEach('Visitando o site e fazendo login', () => {
+    cy.visit('https://www.saucedemo.com');
 
-  it('preencher username, password e login', () => {
-
-    cy.get('[data-test="username"]')
-      .type('standard_user')
-
-    cy.get('[data-test="password"]')
-      .type('secret_sauce')
-
+    // Login com usuário padrão
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]')
       .click()
 
-    cy.get('[data-test="title"]').should('be.visible')
+    // Verifica se o login foi bem-sucedido
+    cy.get('[data-test="title"]').should('be.visible');
+  });
+
+  it('Fazendo compra e logout', () => {
 
     cy.get(':nth-child(3) > [data-test="inventory-item-description"]')
     cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
@@ -27,5 +25,30 @@ describe('template spec', () => {
       .click()
 
     cy.get('[data-test="shopping-cart-link"]').should('be.visible')
-  });
+    cy.get('[data-test="checkout"]')
+      .click()
+
+    //Preencher dados e prosseguir
+    cy.get('[data-test="firstName"]').type('standard')
+    cy.get('[data-test="lastName"]').type('user')
+    cy.get('[data-test="postalCode"]').type('00000-000')
+    cy.get('[data-test="continue"]')
+      .click()
+
+    //Finalizar pedido
+    cy.get('#finish')
+      .click()
+
+    //Voltando a home
+    cy.get('#back-to-products')
+      .click()
+    cy.get('[data-test="inventory-container"]').should('be.visible')
+
+    //Logout da conta
+    cy.get('#react-burger-menu-btn')
+      .click()
+    cy.get('[data-test="logout-sidebar-link"]')
+      .click()
+  })
+
 })
